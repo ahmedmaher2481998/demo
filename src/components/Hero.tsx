a5ea0889943815e4c01faffa8185b23ecc285f0b
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-10-27 12:22:29 +0200

    edits2

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,14 +8,14 @@ const Hero = () => {
         <div className="yellow "></div>
         <div className="blue "></div>.
       </div>
-      <div className="grid grid-cols-2 grid-rows-1 md:ml-10 mt-14 md:mt-32">
+      <div className="grid grid-cols-2 grid-rows-1 md:ml-10 mt-14 md:mt-36">
         <div className="row-span-1 row-start-1">
           <div className="flex items-center">
             <div>
               <h1 className="text-white text-4xl w-[25rem] font-bold">
                 Sign In to Recharge Direct
               </h1>
-              <div className="text-white m-auto text-2xl font-semibold">
+              <div className="text-white m-auto text-xl  mt-10 font-semibold">
                 if you don&apos;t an account you can
                 <span className="text-secondary">
                   <Link to="/sign-up">&nbsp; Register here!</Link>
